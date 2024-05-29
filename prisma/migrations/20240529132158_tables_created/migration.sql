0acfa6f1aa520733072f89fa02d08031c290f4e0
Rohan Kadam <rohan.kadam@mindbowser.com>
2024-05-29 19:26:41 +0530

    init setup

--- prisma/migrations/20240529132158_tables_created/migration.sql
+++ prisma/migrations/20240529132158_tables_created/migration.sql
@@ -16,6 +16,7 @@ CREATE TABLE "Post" (
     "id" TEXT NOT NULL,
     "authorId" TEXT NOT NULL,
     "title" TEXT NOT NULL,
+    "published" BOOLEAN NOT NULL DEFAULT false,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
